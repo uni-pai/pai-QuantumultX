160b4ecf9a63ad05addae105ed98f83d2b6798af
uni-pai <56943441+uni-pai@users.noreply.github.com>
2020-01-24 10:17:33 +0800

    Delete bilibilicr.js

--- Script/bilibilicr.js
+++
@@ -1,7 +0,0 @@
-/*
-^http(s)://app.bilibili.com/v2/playurl.* url script-response-body http://localhost/Script/bilibilicr.js
-*/
-
-var body = JSON.parse($response.body);
-body.quality = body.accept_quality[0];
-$done({body:JSON.stringify(body)});
